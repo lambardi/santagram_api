835d63c506a240f020b821a1002b7d661caa2eeb
Ben Golden <ben@sendgrid.com>
2014-12-07 21:21:53 -0700

    have a little tutorial server working. played with user objects a little, wanted to check in my progress.

--- src/santagram_api.go
+++ src/santagram_api.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 
 func main() {
-	fmt.Printf("Hello world!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+
+//	userIn := &User{Username: "Benjamin", Email: "lambardi@gmail.com"}
+//	userIn.save()
+//	userOut, _ := loadUser("Benjamin")
+//	fmt.Println(string(userOut.Email))
+
+}
+
+type User struct {
+	Username string
+	Email string
+}
+
+func (u *User) save() error {
+	filename := u.Username + ".txt"
+	emailBytes := []byte(fmt.Sprintf("%s\n", u.Email))
+	return ioutil.WriteFile(filename, emailBytes, 0600)
+	}
+
+func loadUser(username string) (*User, error) {
+	filename := username + ".txt"
+	email, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	emailString := string(email[:])
+	return &User{Username: username, Email: emailString}, nil
 }
