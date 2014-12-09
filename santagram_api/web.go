96e6bace7566ebd9859c988d5f325e6c7a35394f
Ben Golden <ben@sendgrid.com>
2014-12-08 21:42:36 -0700

    fuuuuck

---
+++ santagram_api/web.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
+
+//	userIn := &User{Username: "Benjamin", Email: "lambardi@gmail.com"}
+//	userIn.save()
+//	userOut, _ := loadUser("Benjamin")
+//	fmt.Println(string(userOut.Email))
+
+}
+
+type User struct {
+	Username string
+	Email string
+}
+
+func (u *User) save() error {
+	filename := u.Username + ".txt"
+	emailBytes := []byte(fmt.Sprintf("%s\n", u.Email))
+	return ioutil.WriteFile(filename, emailBytes, 0600)
+	}
+
+func loadUser(username string) (*User, error) {
+	filename := username + ".txt"
+	email, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	emailString := string(email[:])
+	return &User{Username: username, Email: emailString}, nil
+}
